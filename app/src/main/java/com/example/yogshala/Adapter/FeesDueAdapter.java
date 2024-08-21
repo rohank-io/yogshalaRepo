d47a8dd639b8696aa3af6fac4f8e131fe0d155f2
Rohan Kudale <rohankudale2110@gmail.com>
2024-08-21 12:26:08 +0530

    Commit of Fees Due

---
+++ app/src/main/java/com/example/yogshala/Adapter/FeesDueAdapter.java
@@ -0,0 +1,53 @@
+package com.example.yogshala.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.yogshala.R;
+import com.example.yogshala.model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeesDueAdapter extends ArrayAdapter<Transaction> {
+    private Context context;
+    private List<Transaction> transactions;
+
+    public FeesDueAdapter(Context context, ArrayList<Transaction> list) {
+        super(context, 0, list);
+        this.context = context;
+        this.transactions = list;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item_fees_due, parent, false);
+        }
+
+        Transaction transaction = transactions.get(position);
+
+
+        TextView tvName = convertView.findViewById(R.id.tvName);
+        TextView tvDueDate = convertView.findViewById(R.id.tvDueDate);
+        TextView tvProgram = convertView.findViewById(R.id.tvProgram);
+
+
+        // Set data from transaction object
+        tvName.setText(transaction.getClientName());
+        tvDueDate.setText(transaction.getToDate());
+        tvProgram.setText(transaction.getProgram());
+
+        // Set program icon and status arrow if needed
+        // ivProgram.setImageResource(transaction.getProgramIcon());
+        // ivStatus.setImageResource(R.drawable.arrowicon); // or another icon based on status
+
+        return convertView;
+    }
+}
+
